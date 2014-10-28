3e742eae11214c5311081bb46390e169b1bd3d56
Zixradoom <Zixradoom@gmail.com>
2014-10-28 09:36:23 -0400

    Add more tests for the SimpleCalculator

--- src/test/java/com/s2d/math/calculator/SimpleCalculatorTest.java
+++ src/test/java/com/s2d/math/calculator/SimpleCalculatorTest.java
@@ -3,6 +3,7 @@ package com.s2d.math.calculator;
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -41,6 +42,34 @@ public class SimpleCalculatorTest
     assertEquals ( "", Integer.valueOf ( 0 ), Integer.valueOf ( compare ) );
   }
   
+  @Test(expected=NullPointerException.class)
+  public void pushNullString ()
+  {
+    String value = null;
+    calculator.push ( value );
+  }
+  
+  @Test(expected=NumberFormatException.class)
+  public void pushEmptyString ()
+  {
+    String value = "";
+    calculator.push ( value );
+  }
+  
+  @Test(expected=NullPointerException.class)
+  public void pushNullBigDecimal ()
+  {
+    BigDecimal value = null;
+    calculator.push ( value );
+  }
+  
+  @Test(expected=NullPointerException.class)
+  public void pushNullBigInteger ()
+  {
+    BigInteger value = null;
+    calculator.push ( value );
+  }
+  
   @Test(expected=CalculatorStackException.class)
   public void absEmptyStack ()
   {
@@ -51,6 +80,7 @@ public class SimpleCalculatorTest
   public void absNegative ()
   {
     calculator.push ( BigDecimal.TEN.negate () );
+    calculator.abs ();
     BigDecimal value = calculator.pop ();
     int compare = BigDecimal.TEN.compareTo ( value );
     assertEquals ( "", Integer.valueOf ( 0 ), Integer.valueOf ( compare ) );
@@ -60,6 +90,7 @@ public class SimpleCalculatorTest
   public void absPositive ()
   {
     calculator.push ( BigDecimal.TEN );
+    calculator.abs ();
     BigDecimal value = calculator.pop ();
     int compare = BigDecimal.TEN.compareTo ( value );
     assertEquals ( "", Integer.valueOf ( 0 ), Integer.valueOf ( compare ) );
@@ -69,8 +100,27 @@ public class SimpleCalculatorTest
   public void absZero ()
   {
     calculator.push ( BigDecimal.ZERO );
+    calculator.abs ();
     BigDecimal value = calculator.pop ();
     int compare = BigDecimal.ZERO.compareTo ( value );
     assertEquals ( "", Integer.valueOf ( 0 ), Integer.valueOf ( compare ) );
+  }
+  
+  @Test(expected=CalculatorStackException.class)
+  public void duplicateEmptyStack ()
+  {
+    calculator.duplicate ();
+  }
+  
+  @Test
+  public void duplicateNumber ()
+  {
+    calculator.push ( "10" );
+    calculator.duplicate ();
+    BigDecimal value = calculator.pop ();
+    int compare1 = BigDecimal.TEN.compareTo ( value );
+    value = calculator.pop ();
+    int compare2 = BigDecimal.TEN.compareTo ( value );
+    assertEquals ( "", Boolean.valueOf ( true ), Boolean.valueOf ( compare1 == 0 && compare2 == 0 ) );
   }
 }
