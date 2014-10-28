ab851848d47e8f029d584da85d91189f96d8240a
Zixradoom <Zixradoom@gmail.com>
2014-10-27 20:38:53 -0400

    Start adding JUnit tests

--- src/main/java/com/s2d/math/util/Random.java
+++ src/main/java/com/s2d/math/util/Random.java
@@ -4,14 +4,14 @@ import java.math.BigDecimal;
 
 public interface Random
 {
-    public void nextBytes ( byte[] bytes );
-    public int nextInt ();
-    public int nextInt ( int n );
-    public long nextLong ();
-    public boolean nextBoolean ();
-    public float nextFloat ();
-    public double nextDouble ();
-    public double nextGaussian ();
+  public void nextBytes ( byte[] bytes );
+  public int nextInt ();
+  public int nextInt ( int n );
+  public long nextLong ();
+  public boolean nextBoolean ();
+  public float nextFloat ();
+  public double nextDouble ();
+  public double nextGaussian ();
 	public BigDecimal nextBigDecimal ();
 	public BigDecimal inRange ( BigDecimal lower, BigDecimal upper );
 }
---
+++ src/test/java/com/s2d/math/util/MathUtilTest.java
@@ -0,0 +1,73 @@
+package com.s2d.math.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class MathUtilTest
+{
+  @Before
+  public void setUpTest ()
+  {
+  }
+  
+  @After
+  public void cleanUpTest ()
+  {
+  }
+  
+  @Test
+  public void clampLowerGreaterThanUpper ()
+  {
+  }
+  
+  @Test
+  public void clampLowerEqualToUpper ()
+  {
+  }
+  
+  @Test
+  public void clampLowerLessThanUpper ()
+  {
+  }
+  
+  @Test
+  public void clampValueLessThanLower ()
+  {
+  }
+  
+  @Test
+  public void clampValueEqualToLower ()
+  {
+  }
+  
+  @Test
+  public void clampValueGreaterThanLower ()
+  {
+  }
+  
+  @Test
+  public void clampValueEqualToUpper ()
+  {
+  }
+  
+  @Test
+  public void clampValueGreaterThanUpper ()
+  {
+  }
+  
+  @Test
+  public void clampValueNull ()
+  {
+  }
+  
+  @Test
+  public void clampLowerNull ()
+  {
+  }
+  
+  @Test
+  public void clampUpperNull ()
+  {
+  }
+}
